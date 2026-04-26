ea85d0673241a41a340f5cc975df33f547a274a0
Julie <juliethron@gmail.com>
2026-04-26 21:12:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+import { getPosts } from "./api/posts.js";
+import { renderPosts } from "./ui/renderPosts.js";
 
+async function init() {
+  const response = await getPosts();
+  renderPosts(response.data);
+}
+
+init();
