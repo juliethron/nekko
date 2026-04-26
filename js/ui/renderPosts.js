ce48cc3dbd780149af2e6f2468128d55cd588b0d
Julie <juliethron@gmail.com>
2026-04-26 21:12:29 +0200

    Update renderPosts.js

--- js/ui/renderPosts.js
+++ js/ui/renderPosts.js
@@ -1,1 +1,17 @@
+export function renderPosts(posts) {
+  const container = document.getElementById("feed");
 
+  container.innerHTML = "";
+
+  posts.forEach(post => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+      <h3>${post.title}</h3>
+      <p>${post.body}</p>
+      <small>By ${post.author.name}</small>
+    `;
+
+    container.appendChild(div);
+  });
+}
