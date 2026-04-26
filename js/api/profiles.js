11b10f33be7533214e92378259eb4ea748231511
Julie <juliethron@gmail.com>
2026-04-26 21:15:40 +0200

    Update profiles.js

--- js/api/profiles.js
+++ js/api/profiles.js
@@ -1,1 +1,23 @@
+import { getToken } from "../utils/storage.js";
 
+const BASE_URL = "https://v2.api.noroff.dev";
+
+
+export async function followUser(name) {
+  await fetch(`${BASE_URL}/social/profiles/${name}/follow`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${getToken()}`
+    }
+  });
+}
+
+
+export async function unfollowUser(name) {
+  await fetch(`${BASE_URL}/social/profiles/${name}/unfollow`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${getToken()}`
+    }
+  });
+}
