f2e72250fc7f1d828c2bc4e4edb1341640fd1813
Julie <juliethron@gmail.com>
2026-04-27 00:18:04 +0200

    Update posts.js

--- js/api/posts.js
+++ js/api/posts.js
@@ -6,12 +6,15 @@ const BASE_URL = "https://v2.api.noroff.dev/social/posts";
 export async function getPosts() {
   const token = localStorage.getItem("token");
 
-  const res = await fetch("https://v2.api.noroff.dev/social/posts", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "X-Noroff-API-Key": "134d87df-3d4c-4578-b111-c34a8e816707"
+  const res = await fetch(
+    "https://v2.api.noroff.dev/social/posts?_author=true&limit=50",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": "134d87df-3d4c-4578-b111-c34a8e816707"
+      }
     }
-  });
+  );
 
   return await res.json();
 }
