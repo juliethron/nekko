4c05a23657e27c74f09b8cfc0b5d44f7deee0d27
Julie <juliethron@gmail.com>
2026-04-27 00:09:16 +0200

    Update renderPosts.js

--- js/ui/renderPosts.js
+++ js/ui/renderPosts.js
@@ -1,6 +1,8 @@
 export function renderPosts(posts) {
   const container = document.getElementById("feed");
 
+  if (!container) return;
+
   container.innerHTML = "";
 
   posts.forEach(post => {
@@ -10,10 +12,13 @@ export function renderPosts(posts) {
     div.innerHTML = `
       <h3>${post.title || "No title"}</h3>
       <p>${post.body || ""}</p>
+
+      <div class="post-actions">
+        <button onclick="deletePost('${post.id}')">Delete</button>
+        <button onclick="editPost('${post.id}')">Edit</button>
+      </div>
     `;
 
     container.appendChild(div);
   });
 }
-
-<button onclick="deletePost('${post.id}')">Delete</button>
