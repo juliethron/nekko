418e4923e2c570b1b6a5bbf3c80d24cf36f11437
Julie <juliethron@gmail.com>
2026-04-27 20:29:03 +0200

    Update renderPosts.js

--- js/ui/renderPosts.js
+++ js/ui/renderPosts.js
@@ -10,13 +10,12 @@ export function renderPosts(posts) {
     div.classList.add("post");
 
     div.innerHTML = `
-  <h3>
-    <a href="/nekko/pages/post.html?id=${post.id}">
-      ${post.title || "No title"}
-    </a>
-  </h3>
-  <p>${post.body || ""}</p>
-`;
+      <h3>
+        <a href="/nekko/pages/post.html?id=${post.id}">
+          ${post.title || "No title"}
+        </a>
+      </h3>
+      <p>${post.body || ""}</p>
 
       <div class="post-actions">
         <button onclick="deletePost('${post.id}')">Delete</button>
