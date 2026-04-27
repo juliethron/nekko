2f795f11baf6f1bead88ff837234f43b2515507e
Julie <juliethron@gmail.com>
2026-04-27 14:23:35 +0200

    Update posts.js

--- js/api/posts.js
+++ js/api/posts.js
@@ -18,19 +18,21 @@ export async function createPost(postData) {
 }
 
 
-export async function createPost(postData) {
-  const res = await fetch(BASE_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken()}`
-    },
-    body: JSON.stringify(postData)
-  });
+eexport async function getPosts() {
+  const token = localStorage.getItem("token");
+
+  const res = await fetch(
+    "https://v2.api.noroff.dev/social/posts?_author=true&limit=50&sort=created&order=desc",
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": "134d87df-3d4c-4578-b111-c34a8e816707"
+      }
+    }
+  );
 
   return await res.json();
 }
-
 
 export async function deletePost(id) {
   await fetch(`${BASE_URL}/${id}`, {
