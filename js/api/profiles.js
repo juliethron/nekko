ea15b2c1e313eeb46b7a18a12760dba84c5d1d2e
Julie <juliethron@gmail.com>
2026-04-27 22:31:29 +0200

    Update profiles.js

--- js/api/profiles.js
+++ js/api/profiles.js
@@ -4,21 +4,41 @@ const BASE_URL = "https://v2.api.noroff.dev";
 const API_KEY = "134d87df-3d4c-4578-b111-c34a8e816707";
 
 export async function followUser(name) {
-  await fetch(`${BASE_URL}/social/profiles/${name}/follow`, {
+  const res = await fetch(`${BASE_URL}/social/profiles/${name}/follow`, {
     method: "PUT",
     headers: {
       Authorization: `Bearer ${getToken()}`,
       "X-Noroff-API-Key": "134d87df-3d4c-4578-b111-c34a8e816707"
     }
   });
+
+  console.log("FOLLOW STATUS:", res.status);
+
+  if (!res.ok) {
+    const error = await res.json();
+    console.error("Follow failed:", error);
+    throw new Error("Follow failed");
+  }
+
+  return await res.json();
 }
 
 export async function unfollowUser(name) {
-  await fetch(`${BASE_URL}/social/profiles/${name}/unfollow`, {
+  const res = await fetch(`${BASE_URL}/social/profiles/${name}/unfollow`, {
     method: "PUT",
     headers: {
       Authorization: `Bearer ${getToken()}`,
       "X-Noroff-API-Key": "134d87df-3d4c-4578-b111-c34a8e816707"
     }
   });
+
+  console.log("UNFOLLOW STATUS:", res.status);
+
+  if (!res.ok) {
+    const error = await res.json();
+    console.error("Unfollow failed:", error);
+    throw new Error("Unfollow failed");
+  }
+
+  return await res.json();
 }
