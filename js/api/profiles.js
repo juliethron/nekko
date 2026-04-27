313da9971f67acce432c17f8f216ebbc8b4dcd50
Julie <juliethron@gmail.com>
2026-04-27 19:38:49 +0200

    Update profiles.js

--- js/api/profiles.js
+++ js/api/profiles.js
@@ -1,23 +1,24 @@
 import { getToken } from "../utils/storage.js";
 
 const BASE_URL = "https://v2.api.noroff.dev";
-
+const API_KEY = "134d87df-3d4c-4578-b111-c34a8e816707";
 
 export async function followUser(name) {
   await fetch(`${BASE_URL}/social/profiles/${name}/follow`, {
     method: "PUT",
     headers: {
-      Authorization: `Bearer ${getToken()}`
+      Authorization: `Bearer ${getToken()}`,
+      "X-Noroff-API-Key": "134d87df-3d4c-4578-b111-c34a8e816707"
     }
   });
 }
-
 
 export async function unfollowUser(name) {
   await fetch(`${BASE_URL}/social/profiles/${name}/unfollow`, {
     method: "PUT",
     headers: {
-      Authorization: `Bearer ${getToken()}`
+      Authorization: `Bearer ${getToken()}`,
+      "X-Noroff-API-Key": "134d87df-3d4c-4578-b111-c34a8e816707"
     }
   });
 }
