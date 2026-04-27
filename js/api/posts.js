0942aaa8fdcaf9a270a4c88c1ca183be82ad6671
Julie <juliethron@gmail.com>
2026-04-27 14:20:27 +0200

    Update posts.js

--- js/api/posts.js
+++ js/api/posts.js
@@ -3,18 +3,16 @@ import { getToken } from "../utils/storage.js";
 const BASE_URL = "https://v2.api.noroff.dev/social/posts";
 
 
-export async function getPosts() {
-  const token = localStorage.getItem("token");
-
-  const res = await fetch(
-    "https://v2.api.noroff.dev/social/posts?_author=true&limit=50",
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "X-Noroff-API-Key": "134d87df-3d4c-4578-b111-c34a8e816707"
-      }
-    }
-  );
+export async function createPost(postData) {
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()}`,
+      "X-Noroff-API-Key": "134d87df-3d4c-4578-b111-c34a8e816707"
+    },
+    body: JSON.stringify(postData)
+  });
 
   return await res.json();
 }
@@ -38,7 +36,8 @@ export async function deletePost(id) {
   await fetch(`${BASE_URL}/${id}`, {
     method: "DELETE",
     headers: {
-      Authorization: `Bearer ${getToken()}`
+      Authorization: `Bearer ${getToken()}`,
+      "X-Noroff-API-Key": "134d87df-3d4c-4578-b111-c34a8e816707"
     }
   });
 }
